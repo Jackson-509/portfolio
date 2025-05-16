56d44802c08c3a73d5c962b6d03c3a8bcfb79ab9
Jackson-509 <info.aristil.jackson@gmail.com>
2025-05-16 14:49:40 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,37 +4,3 @@ function scrollToSection(sectionId) {
         behavior: 'smooth'
     });
 }
-<script>
-  const imageSets = {
-    "boite-mail": [
-      "image_source_portfolio/power bi.png",
-      "image_source_portfolio/details_assitantes.png",
-      "image_source_portfolio/boite_mail_3.png"
-    ],
-    // Tu peux en ajouter d'autres ici
-  };
-
-  let currentImageIndex = 0;
-  let currentSet = [];
-
-  function openLightbox(setKey) {
-    currentSet = imageSets[setKey];
-    currentImageIndex = 0;
-    document.getElementById("lightbox-img").src = currentSet[0];
-    document.getElementById("lightbox").style.display = "flex";
-  }
-
-  function closeLightbox() {
-    document.getElementById("lightbox").style.display = "none";
-  }
-
-  function nextImage() {
-    currentImageIndex = (currentImageIndex + 1) % currentSet.length;
-    document.getElementById("lightbox-img").src = currentSet[currentImageIndex];
-  }
-
-  function prevImage() {
-    currentImageIndex = (currentImageIndex - 1 + currentSet.length) % currentSet.length;
-    document.getElementById("lightbox-img").src = currentSet[currentImageIndex];
-  }
-</script>
